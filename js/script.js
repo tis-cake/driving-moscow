97f3c8822b8fcde7fdcef32d19ecdee946282d5d
tis <tisbootis@gmail.com>
2020-04-14 22:55:23 +0300

    add js for file on form-callManager

--- js/script.js
+++ js/script.js
@@ -589,3 +589,20 @@ $(document).ready(function () {
     scrollPrev = scrolled;
   })
 });
+
+// label с файлом
+let inputs = document.querySelectorAll('.vacancies-file');
+Array.prototype.forEach.call(inputs, function (input) {
+  let label = document.querySelector('.more-content-btn'),
+    labelVal = label.querySelector('.input__file-button-text').innerText;
+  input.addEventListener('change', function (e) {
+    let countFiles = '';
+    if (this.files && this.files.length >= 1)
+      countFiles = this.files.length;
+
+    if (countFiles)
+      label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+    else
+      label.querySelector('.input__file-button-text').innerText = labelVal;
+  });
+});
